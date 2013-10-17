89afc07ef94e8ea096827682e55c2c1669a12a40
Mark Nunberg <mnunberg@haskalah.org>
2013-10-16 17:52:07 -0700

    Moved
    Change-Id: I05e2fd32a0be78aaa29650c084871e0f84547d3d

--- gcouchbase/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-from couchbase.tests import *
--- txcouchbase/__init__.py
+++
@@ -1,7 +0,0 @@
-from txcouchbase.connection import Connection
-__version__ = '0.1.0'
-
-class TxCouchbase(object):
-    @classmethod
-    def connect(cls, *args, **kwargs):
-        return Connection(*args, **kwargs)
